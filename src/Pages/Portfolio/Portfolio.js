9a3148dafd33389978144485417b66061f3caaf3
pedromonteiro01 <pmapm@ua.pt>
2022-05-14 11:08:50 +0100

    start portfolio page

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -1,14 +1,32 @@
 import "./Portfolio.css";
 import Background from "../../Components/Background/Background";
+import ListHeader from "../../Components/ListView/ListHeader/ListHeader";
+import ListView from "../../Components/ListView/ListView";
 
 function Portfolio() {
   return (
     <>
       <Background
-        class='background-no-margin'
+        class='background'
         title="Portfolio"
         desc=""
       />
+      <ListView>
+      <div>
+        <ListHeader
+              class="portfolio-header-item"
+              department="Serviços"
+        />
+        <ListHeader
+              class="portfolio-header-item"
+              department="Iniciativas"
+        />
+        <ListHeader
+              class="portfolio-header-item"
+              department="Responsabilidades Sociais"
+        />
+      </div>
+      </ListView>
     </>
   );
 }

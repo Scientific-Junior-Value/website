3e8ded4cc41450af12fb34f445b110fd7b0fbcf3
pedromonteiro01 <pmapm@ua.pt>
2022-05-31 20:09:46 +0100

    update styles for carousel

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,11 +1,14 @@
 import './Card.css';
 import logo from './dominos.png';
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div className='card'>
                 <div className='card-image'>
-                    <img src={logo}></img>
+                    <img src={props.image}></img>
+                </div>
+                <div className='card-legend'>
+                    <p>{props.name}</p>
                 </div>
         </div>
     );
--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -10,31 +10,33 @@ import './Carousel.css';
 
 const Carousel = () => {
     SwiperCore.use([Autoplay]);
+
     return (
         <div className='carousel'>
             <Swiper className='swiper-container'
-                    autoplay={{ delay: 1000 }}
+                    autoplay={{ delay: 1800 }}
                   modules={[Navigation, Pagination, Scrollbar, A11y]}
                   spaceBetween={0}
                   loop={true}
-                  slidesPerView={7}
+                  slidesPerView={5}
                   grabCursor={true}
                   fade={true}    
-                    delay= {2000}
-                  pagination={true}     
+                    pagination={{clickable: true}}    
             >
-            <SwiperSlide><Card /></SwiperSlide>
-            <SwiperSlide><Card /></SwiperSlide>
-            <SwiperSlide><Card /></SwiperSlide>
-            <SwiperSlide><Card /></SwiperSlide>
-            <SwiperSlide><Card /></SwiperSlide>
-            <SwiperSlide><Card /></SwiperSlide>
-            <SwiperSlide><Card /></SwiperSlide>
-            <SwiperSlide><Card /></SwiperSlide>
-            <SwiperSlide><Card /></SwiperSlide>
-            <SwiperSlide><Card /></SwiperSlide>
-            <SwiperSlide><Card /></SwiperSlide>
-            <SwiperSlide><Card /></SwiperSlide>
+
+            <SwiperSlide><Card name="Dominos" image="parcerias/dominos.png" /></SwiperSlide>
+            <SwiperSlide><Card name="Iron Duke" image="parcerias/ironduke.jpg"/></SwiperSlide>
+            <SwiperSlide><Card name="IPDJ" image="parcerias/ipdj.png"/></SwiperSlide>
+            <SwiperSlide><Card name="Academia Génios Livres" image="parcerias/gl.jpg"/></SwiperSlide>
+            <SwiperSlide><Card name="Aveiro Smart Business" image="parcerias/aveirosmart.png"/></SwiperSlide>
+            <SwiperSlide><Card name="Universidade de Aveiro" image="parcerias/ua.png"/></SwiperSlide>
+            <SwiperSlide><Card name="Carla Pereira" image="parcerias/Carla.png"/></SwiperSlide>
+            <SwiperSlide><Card name="Eduardo Espinheira" image="parcerias/eduardo.jpg"/></SwiperSlide>
+            <SwiperSlide><Card name="Ginásio da Educação Da Vinci" image="parcerias/ginasiodaeducaçãodavinci.png"/></SwiperSlide>
+            <SwiperSlide><Card name="Henrique Girão" image="parcerias/henrique.jpeg"/></SwiperSlide>
+            <SwiperSlide><Card name="Milagre Coffee Shop" image="parcerias/milagre.jpg"/></SwiperSlide>
+            <SwiperSlide><Card name="Gráfica Aveirense" image="parcerias/grafica.jpg"/></SwiperSlide>
+            <SwiperSlide><Card name="Impresspoint" image="parcerias/impresspoint.png"/></SwiperSlide>
             </Swiper>
         </div>
     );
